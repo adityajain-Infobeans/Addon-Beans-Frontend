4859fcf2d7ef53c6db7e10a98e8cd6e73ae6e693
Aditya Jain - Developer Indore <adityan.jain@infobeans.com>
2021-05-21 19:33:09 +0530

    client express route file

---
+++ back-end/route/client.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const Client = require('../models/Client');
+
+router.get('/', function (req, res) {
+    // show all clients code here
+    res.send('
+    ');
+});
+module.exports = router;
