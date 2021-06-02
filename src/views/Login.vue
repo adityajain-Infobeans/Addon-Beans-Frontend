efdf66cfc02486facad3047c23355857299f8d10
Aditya Jain - Developer Indore <adityan.jain@infobeans.com>
2021-06-02 19:49:07 +0530

    basic login post request with axios

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -13,6 +13,7 @@
             id="Email"
             prepend-icon="mdi-account-circle"
             type="email"
+            v-model="email"
           ></v-text-field>
           <v-text-field
             name="Password"
@@ -22,28 +23,51 @@
             prepend-icon="mdi-lock"
             :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
             @click:append="showPassword = !showPassword"
+            v-model="password"
           ></v-text-field>
         </v-form>
       </v-card-text>
       <v-divider></v-divider>
       <v-card-actions>
-        <v-btn color="success">Registration </v-btn>
+        <!-- <v-btn color="success">Registration </v-btn> -->
         <v-spacer></v-spacer>
-        <v-btn color="info">Login </v-btn>
+        <v-btn color="info" @click="login">Login </v-btn>
       </v-card-actions>
     </v-card>
   </div>
 </template>
 
 <script>
+const axios = require('axios');
+
 export default {
   data: () => ({
     showPassword: false,
+    email: null,
+    password: null,
   }),
 
   mounted() {
-    document.getElementById('app').style.backgroundColor = '#FFCC80';
     document.title = 'Login';
+  },
+
+  methods: {
+    login() {
+      if (this.email && this.password) {
+        axios
+          .post('https://infobeans-support.herokuapp.com/employee', {
+            email: this.email,
+            password: this.password,
+          })
+          .then((response) => {
+            console.log(response);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+      return true;
+    },
   },
 };
 </script>
