5cdf49e2ea11c37efec5fbe406fdd25beae5fc3e
Aditya Jain - Developer Indore <adityan.jain@infobeans.com>
2021-06-02 19:47:50 +0530

    new public path for build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/Support-Ticket-System/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/Support-Ticket-System-Frontend/' : '/',
 
   transpileDependencies: ['vuetify'],
 };
