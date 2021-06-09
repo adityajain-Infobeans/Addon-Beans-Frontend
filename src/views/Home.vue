e9eefd32ac5b5aa76a409cd16487b3dd46d97a66
Aditya Jain - Developer Indore <adityan.jain@infobeans.com>
2021-06-09 17:31:51 +0530

    routing to component instead of conditional render

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,19 @@
 /* eslint-disable vue/no-unused-components */
 <template>
-  <div>
-    <div v-if="isLogin()"><Dashboard /></div>
-    <div v-else><Login /></div>
-  </div>
+  <div></div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Dashboard from '@/views/Dashboard.vue';
-import Login from '@/views/Login.vue';
 
 export default {
   name: 'Home',
-  components: {
-    Dashboard,
-    Login,
-  },
-  methods: {
-    isLogin() {
-      if (!localStorage.getItem('token')) {
-        return false;
-      }
-      return true;
-    },
+
+  mounted() {
+    if (!localStorage.getItem('token')) {
+      this.$router.push({ name: 'Login' });
+    }
+    this.$router.push({ name: 'Dashboard' });
   },
 };
 </script>
