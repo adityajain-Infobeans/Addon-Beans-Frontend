24aa0a538d78d9b1c1a7962ea5459b898711bc5c
Aditya Jain - Developer Indore <adityan.jain@infobeans.com>
2021-06-10 15:41:56 +0530

    made component mobile friendly

--- src/components/Forms/LoginForm.vue
+++ src/components/Forms/LoginForm.vue
@@ -1,45 +1,55 @@
 <template>
-  <v-card width="600" class="mx-auto mt-12">
-    <v-card-title>
-      <h1 class="display-1">Login</h1>
-    </v-card-title>
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="8" lg="6" xl="5" class="mx-auto">
+        <v-card class="mx-auto mt-12">
+          <v-card-title>
+            <h1 class="display-1">Login</h1>
+          </v-card-title>
 
-    <v-card-text>
-      <v-alert type="error" v-if="errorMessage">{{ errorMessage }}</v-alert>
-      <v-alert type="success" v-else-if="successMessage">Login Success</v-alert>
-      <v-form v-model="formValidated">
-        <v-text-field
-          name="email"
-          label="Email"
-          id="Email"
-          prepend-icon="mdi-account-circle"
-          type="email"
-          v-model="email"
-          :rules="emailRules"
-          @keyup.enter="login"
-        ></v-text-field>
-        <v-text-field
-          name="Password"
-          label="Password"
-          id="Password"
-          :type="showPassword ? 'text' : 'password'"
-          prepend-icon="mdi-lock"
-          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-          @click:append="showPassword = !showPassword"
-          v-model="password"
-          :rules="passwordRules"
-          @keyup.enter="login"
-        ></v-text-field>
-      </v-form>
-    </v-card-text>
-    <v-divider></v-divider>
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn color="info" @click="login" :disabled="!formValidated"
-        >Login
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+          <v-card-text>
+            <v-alert type="error" v-if="errorMessage">{{
+              errorMessage
+            }}</v-alert>
+            <v-alert type="success" v-else-if="successMessage"
+              >Login Success</v-alert
+            >
+            <v-form v-model="formValidated">
+              <v-text-field
+                name="email"
+                label="Email"
+                id="Email"
+                prepend-icon="mdi-account-circle"
+                type="email"
+                v-model="email"
+                :rules="emailRules"
+                @keyup.enter="login"
+              ></v-text-field>
+              <v-text-field
+                name="Password"
+                label="Password"
+                id="Password"
+                :type="showPassword ? 'text' : 'password'"
+                prepend-icon="mdi-lock"
+                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="showPassword = !showPassword"
+                v-model="password"
+                :rules="passwordRules"
+                @keyup.enter="login"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="info" @click="login" :disabled="!formValidated"
+              >Login
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
