2b38a817db410e189cf22b3e16eb8a2fd70bb1b6
Aditya Jain - Infobeans <adityan.jain@infobeans.com>
2021-06-22 18:42:49 +0530

    updating prod folder url

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/Support-Requirement-System-Frontend/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/Addon-Beans-Frontend/' : '/',
 
   transpileDependencies: ['vuetify'],
 };
