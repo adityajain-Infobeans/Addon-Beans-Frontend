982d77347b358397cc025d93d08ac41e33abf4db
Aditya Jain Infobeans <adityan.jain@infobeans.com>
2021-07-12 18:31:02 +0530

    vue add @vue/unit-jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,16 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: ['plugin:vue/essential', '@vue/airbnb'],
+
   parserOptions: {
     parser: 'babel-eslint',
   },
+
   rules: {
     'linebreak-style': 0,
     'implicit-arrow-linebreak': 0,
@@ -14,4 +18,16 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
