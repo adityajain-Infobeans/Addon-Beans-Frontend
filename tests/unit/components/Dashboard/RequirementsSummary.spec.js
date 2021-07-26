259e79192145692ca256038a92dc5a7acfa3ade2
Aditya Jain Infobeans <adityan.jain@infobeans.com>
2021-07-26 18:54:39 +0530

    fail test with API calls: 0

--- tests/unit/components/Dashboard/RequirementsSummary.spec.js
+++ tests/unit/components/Dashboard/RequirementsSummary.spec.js
@@ -1,9 +1,30 @@
+const { shallowMount } = require('@vue/test-utils');
+const Summary = require('@/components/Dashboard/RequirementsSummary.vue');
+const { getSummaryData } = require('@/services/axios/Dashboard/RequirementsSummary');
+const flushPromises = require('flush-promises');
+
+jest.mock('@/services/axios/Dashboard/RequirementsSummary');
+
 describe('FullComponentTest', () => {
-  it('Verify default values', () => {
-    expect(true).toBe(true);
+  it('Api call resolved successfully', async () => {
+    resolvedData = {
+      status: 'success',
+      message: 'Data retrieved successfully',
+      data: { totalRequirements: 2, openRequirement: 2, resolvedRequirement: 0 },
+    };
+    getSummaryData.mockResolvedValueOnce({ data: resolvedData });
+
+    const wrapper = shallowMount(Summary);
+
+    await flushPromises();
+
+    expect(getSummaryData).toHaveBeenCalledTimes(1);
+    const totalRequirements = wrapper.find('[data-testid="totalRequirements"]');
+
+    expect(totalRequirements).toEqual(resolvedData);
   });
 
-  it('Checks API call', () => {
+  it('Api call resolved failed', () => {
     expect(true).toBe(true);
   });
 });
