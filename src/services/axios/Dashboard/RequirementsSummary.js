7937277641f57b86cddab8f7850163f21dcdbdfb
Aditya Jain Infobeans <adityan.jain@infobeans.com>
2021-07-27 10:53:02 +0530

    cleaning code

--- src/services/axios/Dashboard/RequirementsSummary.js
+++ src/services/axios/Dashboard/RequirementsSummary.js
@@ -2,18 +2,12 @@
 const axios = require('axios');
 
 export function getSummaryData(token) {
-  return new Promise((resolve, reject) => {
-    axios
-      .get('/summary', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((response) => {
-        resolve(response);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+  return axios
+    .get('/summary', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => response)
+    .catch((error) => error);
 }
