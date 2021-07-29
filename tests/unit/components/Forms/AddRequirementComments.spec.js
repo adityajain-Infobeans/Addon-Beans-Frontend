6c47eb0e87ce840274667ee75e7d37c832d20759
Aditya Jain Infobeans <adityan.jain@infobeans.com>
2021-07-29 18:14:44 +0530

    is HR true check

--- tests/unit/components/Forms/AddRequirementComments.spec.js
+++ tests/unit/components/Forms/AddRequirementComments.spec.js
@@ -93,9 +93,61 @@ describe('FullComponentTest', () => {
     const isHRCheck = wrapper.find('[data-testid="isHRCheck"]');
     expect(isHRCheck.exists()).toBe(false);
 
-    // hide
+    // hide submit button initially
     const commentSubmitBtn = wrapper.find('[data-testid="commentSubmitBtn"]');
     expect(commentSubmitBtn.exists()).toBe(false);
+  });
+
+  it('shows HR specific option', () => {
+    // setting dummy route for component created method
+    const $route = {
+      params: {
+        id: '1',
+      },
+    };
+
+    const resolvePromise = new Promise((resolve, reject) => {
+      const receivedRequirementIdData = {
+        status: 'success',
+        message: 'Requirement data successfully retrieved',
+        data: {
+          requirement_id: 1,
+          emp_id: 2,
+          status: 1,
+          timeline: 2,
+          additional_note: null,
+          skill_set: '[8,6]',
+          experience: 2,
+          number_of_position: 3,
+          updated_on: '27-07-2021 by Aditya Jain',
+          created_on: '22-06-2021 by Aditya Jain',
+          client_id: 2,
+        },
+      };
+      resolve(receivedRequirementIdData);
+    });
+
+    getRequirementIdData.mockResolvedValueOnce(resolvePromise);
+
+    const wrapper = shallowMount(commentField, {
+      localVue,
+      store,
+      computed: {
+        isHR() {
+          return true;
+        },
+      },
+      mocks: {
+        $route,
+      },
+      propsData: {
+        type: undefined,
+      },
+    });
+
+    // Dont show change status btn to non HR users
+    const isHRCheck = wrapper.find('[data-testid="isHRCheck"]');
+    expect(isHRCheck.exists()).toBe(true);
   });
 
   it('Verify all input fields are present', () => {
