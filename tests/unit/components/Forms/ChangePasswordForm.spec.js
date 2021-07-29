f838a3cfd6bd5cdcd42e0dd40fdffd47dde32e54
Aditya Jain Infobeans <adityan.jain@infobeans.com>
2021-07-29 18:52:13 +0530

    setting up test

--- tests/unit/components/Forms/ChangePasswordForm.spec.js
+++ tests/unit/components/Forms/ChangePasswordForm.spec.js
@@ -1,9 +1,24 @@
+//  import required stuff from vue/test-utils
+import { shallowMount } from '@vue/test-utils';
+
+// import component to mock
+import ChangePasswordForm from '@/components/Forms/ChangePasswordForm.vue';
+
+// import API modules
+const { changePassword } = require('@/services/axios/Forms/ChangePasswordForm');
+
+// mock API modules imported above
+jest.mock('@/services/axios/Forms/ChangePasswordForm');
+
+// clear mocks before each test
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
 describe('FullComponentTest', () => {
   it('Verify default values', () => {
     expect(true).toBe(true);
   });
 
-  it('Verify all input fields are present', () => {
-    expect(true).toBe(true);
-  });
+  it('Verify all input fields are present', () => {});
 });
