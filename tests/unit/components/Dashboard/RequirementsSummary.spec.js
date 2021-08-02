bb4ffa9d0d233bbfeff1faa04dc7d7c3d48ad59e
Aditya Jain Infobeans <adityan.jain@infobeans.com>
2021-08-02 19:36:33 +0530

    setting up summary  test

--- tests/unit/components/Dashboard/RequirementsSummary.spec.js
+++ tests/unit/components/Dashboard/RequirementsSummary.spec.js
@@ -1,18 +1,35 @@
-// const { shallowMount } = require('@vue/test-utils');
-// const Summary = require('@/components/Dashboard/RequirementsSummary.vue');
-// const { getSummaryData } = require('@/services/axios/Dashboard/RequirementsSummary');
+//  import required stuff from vue/test-utils
+import { shallowMount, createLocalVue } from '@vue/test-utils';
 
-// jest.mock('@/services/axios/Dashboard/RequirementsSummary');
-// beforeEach(() => {
-//   jest.clearAllMocks();
-// });
+// import router, vuex & other dependencies
+import Vuex from 'vuex';
 
-describe('FullComponentTest', () => {
-  it('Api call resolved successfully', () => {
-    expect(true).toBe(true);
-  });
+// import component to mock
+import Summary from '@/components/Dashboard/RequirementsSummary.vue';
 
-  it('Api call resolved failed', () => {
-    expect(true).toBe(true);
-  });
+import flushPromises from 'flush-promises';
+
+const { getSummaryData } = require('@/services/axios/Dashboard/RequirementsSummary');
+
+jest.mock('@/services/axios/Dashboard/RequirementsSummary');
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    Auth: {
+      userData: {
+        emp_id: 2,
+        emp_name: 'Test User',
+        emp_email: 'test@email.com',
+        token: 'JWT_token',
+        is_login: true,
+      },
+    },
+  },
 });
