2b9ca2175e35c98558e7859426b718e60f836c78
Aditya Jain Infobeans <adityan.jain@infobeans.com>
2021-08-03 11:27:55 +0530

    Initial setup for component

--- tests/unit/components/Forms/RequirementComments.spec.js
+++ tests/unit/components/Forms/RequirementComments.spec.js
@@ -1,3 +1,39 @@
+//  import required stuff from vue/test-utils
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+
+// import router, vuex & other dependencies
+import Vuex from 'vuex';
+
+// import component to mock
+import RequirementComments from '@/components/Forms/RequirementComments.vue';
+
+import flushPromises from 'flush-promises';
+
+const { getAllComment } = require('@/services/axios/Forms/RequirementComments');
+
+jest.mock('@/services/axios/Forms/RequirementComments');
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    Auth: {
+      userData: {
+        emp_id: 2,
+        emp_name: 'Test User',
+        emp_email: 'test@email.com',
+        token: 'JWT_token',
+        is_login: true,
+      },
+    },
+  },
+});
+
 describe('FullComponentTest', () => {
   it('Verify default values', () => {
     expect(true).toBe(true);
