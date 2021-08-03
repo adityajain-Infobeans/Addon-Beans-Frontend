bf75b48d61566eae69ded65d0fc976f50eec0c18
Aditya Jain Infobeans <adityan.jain@infobeans.com>
2021-08-03 19:41:34 +0530

    submit comment success

--- tests/unit/components/Forms/AddRequirementComments.spec.js
+++ tests/unit/components/Forms/AddRequirementComments.spec.js
@@ -239,4 +239,69 @@ describe('FullComponentTest', () => {
   //   expect(commentSubmitBtn.exists()).toBe(true);
   //   expect(commentSubmitBtnStatus).toBe('false');
   // });
+
+  it('submit comment', () => {
+    // setting dummy route for component created method
+    const $route = {
+      params: {
+        id: '1',
+      },
+    };
+
+    mock.onGet('/Requirement/1').reply(200, {
+      status: 'success',
+      message: 'Requirement data successfully retrieved',
+      data: {
+        requirement_id: 1,
+        emp_id: 2,
+        status: 1,
+        timeline: 2,
+        additional_note: null,
+        skill_set: '[8,6]',
+        experience: 2,
+        number_of_position: 3,
+        updated_on: '27-07-2021 by Aditya Jain',
+        created_on: '22-06-2021 by Aditya Jain',
+        client_id: 2,
+      },
+    });
+
+    mock.onPost('/comment/').reply(200, {
+      status: 'success',
+      message: 'Comment created successfully',
+      data: {
+        comment_id: 3,
+        emp_id: 2,
+        created_on: '03-08-2021 by Aditya Jain',
+        updated_on: '03-08-2021 by Aditya Jain',
+        comment_by: 'Aditya Jain',
+        requirement_id: '12',
+        comment: 'ssssssssssssssssssssssssssssssssssssssssssss',
+      },
+    });
+
+    const wrapper = shallowMount(commentField, {
+      localVue,
+      store,
+      computed: {
+        isHR() {
+          return false;
+        },
+      },
+      mocks: {
+        $route,
+      },
+      propsData: {
+        type: undefined,
+      },
+    });
+
+    // Don't show change status btn to non HR users
+    const isHRCheck = wrapper.find('[data-testid="isHRCheck"]');
+    expect(isHRCheck.exists()).toBe(false);
+
+    // hide submit button initially
+    const commentSubmitBtn = wrapper.find('[data-testid="commentSubmitBtn"]');
+    expect(commentSubmitBtn.exists()).toBe(false);
+  });
 });
