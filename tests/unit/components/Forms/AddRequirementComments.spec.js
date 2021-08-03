224acb5ee6ac043c48373e6ed53edd618796529c
Aditya Jain Infobeans <adityan.jain@infobeans.com>
2021-08-03 19:38:10 +0530

    disable status cange button if status is not changed

--- tests/unit/components/Forms/AddRequirementComments.spec.js
+++ tests/unit/components/Forms/AddRequirementComments.spec.js
@@ -130,4 +130,59 @@ describe('FullComponentTest', () => {
     const isHRCheck = wrapper.find('[data-testid="isHRCheck"]');
     expect(isHRCheck.exists()).toBe(true);
   });
+
+  it('disable change status button for HR', async () => {
+    // setting dummy route for component created method
+    const $route = {
+      params: {
+        id: '1',
+      },
+    };
+
+    mock.onGet('/Requirement/1').reply(200, {
+      status: 'success',
+      message: 'Requirement data successfully retrieved',
+      data: {
+        requirement_id: 1,
+        emp_id: 2,
+        status: 1,
+        timeline: 2,
+        additional_note: null,
+        skill_set: '[8,6]',
+        experience: 2,
+        number_of_position: 3,
+        updated_on: '27-07-2021 by Aditya Jain',
+        created_on: '22-06-2021 by Aditya Jain',
+        client_id: 2,
+      },
+    });
+
+    const wrapper = shallowMount(commentField, {
+      localVue,
+      store,
+      computed: {
+        isHR() {
+          return true;
+        },
+      },
+      mocks: {
+        $route,
+      },
+      propsData: {
+        type: undefined,
+      },
+    });
+
+    await wrapper.setData({ status: 1, currentStatus: 1 });
+
+    // Don't show change status btn to non HR users
+    const isHRCheck = wrapper.find('[data-testid="isHRCheck"]');
+    expect(isHRCheck.exists()).toBe(true);
+
+    // hide submit button initially
+    const commentSubmitBtn = wrapper.find('[data-testid="commentSubmitBtn"]');
+    const commentSubmitBtnStatus = !!commentSubmitBtn.attributes().disabled;
+    expect(commentSubmitBtn.exists()).toBe(true);
+    expect(commentSubmitBtnStatus).toBe(true);
+  });
 });
