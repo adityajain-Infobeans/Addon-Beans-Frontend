d2686a83078667a9313c70707b5fea4e995a7ceb
Aditya Jain Infobeans <adityan.jain@infobeans.com>
2021-08-04 18:48:25 +0530

    verifying initial state

--- tests/unit/components/Forms/ChangePasswordForm.spec.js
+++ tests/unit/components/Forms/ChangePasswordForm.spec.js
@@ -8,4 +8,20 @@ import MockAdapter from 'axios-mock-adapter';
 // import component to mock
 import ChangePasswordForm from '@/components/Forms/ChangePasswordForm.vue';
 
-describe('FullComponentTest', () => {});
+describe('FullComponentTest', () => {
+  it('Verify default values', () => {
+    const wrapper = shallowMount(ChangePasswordForm);
+
+    const newPassword = wrapper.find('[data-testid="newPassword"]');
+    const confirmPassword = wrapper.find('[data-testid="ConfirmPassword"]');
+    const changePasswordBtn = wrapper.find('[data-testid="changePasswordBtn"]');
+    const changePasswordBtnStatus = changePasswordBtn.attributes().disabled;
+    const missMatchAlert = wrapper.find('#swal2-title');
+
+    expect(newPassword.exists()).toBe(true);
+    expect(confirmPassword.exists()).toBe(true);
+    expect(changePasswordBtn.exists()).toBe(true);
+    expect(changePasswordBtnStatus).toBe('true');
+    expect(missMatchAlert.exists()).toBe(false);
+  });
+});
