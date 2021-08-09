0b5860f0d3a333ff993a771221fce69cf235c5ea
Aditya Jain Infobeans <adityan.jain@infobeans.com>
2021-08-09 17:05:25 +0530

    new changePassword axios

--- src/services/axios/Forms/ChangePasswordForm.js
+++ src/services/axios/Forms/ChangePasswordForm.js
@@ -1,19 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 const axios = require('axios');
 
-export function changePassword(token, newPassword) {
-  return axios
-    .put(
-      '/employee',
-      {
-        newPassword,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      },
-    )
-    .then((response) => response)
-    .catch((error) => error);
+export function changePassword(newPassword) {
+  return axios.put('/employee', newPassword);
 }
