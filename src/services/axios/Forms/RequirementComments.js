6654b267ebec0e36422b3055a66c87c10dc04f39
Aditya Jain Infobeans <adityan.jain@infobeans.com>
2021-08-09 17:08:14 +0530

    new getComment code

--- src/services/axios/Forms/RequirementComments.js
+++ src/services/axios/Forms/RequirementComments.js
@@ -1,13 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 const axios = require('axios');
 
-export function getComment(token, RequirementId) {
-  return axios
-    .get(`/comment/T_${RequirementId}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-    .then((response) => response)
-    .catch((error) => error);
+export function getComment(RequirementId) {
+  return axios.get(`/comment/T_${RequirementId}`);
 }
