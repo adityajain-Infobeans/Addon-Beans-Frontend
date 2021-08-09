f5f357da1a73503c0c88550d146999950ebbb6c5
Aditya Jain Infobeans <adityan.jain@infobeans.com>
2021-08-09 19:03:45 +0530

    new axiox middle ware

--- src/services/axios/index.js
+++ src/services/axios/index.js
@@ -1,6 +1,24 @@
 import axios from 'axios';
 import store from '../../store';
 
+const { getSummaryData } = require('@/services/axios/Dashboard/RequirementsSummary.js');
+const {
+  postComment,
+  getRequirementIdData,
+  updateStatus,
+} = require('@/services/axios/Forms/AddRequirementComments.js');
+const { changePassword } = require('@/services/axios/Forms/ChangePasswordForm.js');
+const { loginEmployee } = require('@/services/axios/Forms/LoginForm.js');
+const { getComment } = require('@/services/axios/Forms/RequirementComments.js');
+const { postRequirement, updateRequirement } = require('@/services/axios/Forms/RequirementForm.js');
+const {
+  getRequirementData,
+  getSkillsetData,
+  getClientsData,
+  getEmployeesData,
+  deleteRequirementData,
+} = require('@/services/axios/Table/RequirementsTable.js');
+
 axios.interceptors.request.use(
   (config) => {
     const token = store.getters['Auth/token'];
@@ -13,10 +31,19 @@ axios.interceptors.request.use(
   (error) => Promise.reject(error),
 );
 
-export function getSummaryData() {
-  return axios.get('/summary');
-}
-
-export function postComment(ApiData) {
-  return axios.post('/comment', ApiData);
-}
+export const ApiEndpoint = {
+  getSummaryData,
+  postComment,
+  getRequirementIdData,
+  updateStatus,
+  changePassword,
+  loginEmployee,
+  getComment,
+  postRequirement,
+  updateRequirement,
+  getRequirementData,
+  getSkillsetData,
+  getClientsData,
+  getEmployeesData,
+  deleteRequirementData,
+};
