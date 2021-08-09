340a1e7e263695f231738747cca8e016bca50395
Aditya Jain Infobeans <adityan.jain@infobeans.com>
2021-08-09 17:08:50 +0530

    new postRequirement, updateRequirement code

--- src/services/axios/Forms/RequirementForm.js
+++ src/services/axios/Forms/RequirementForm.js
@@ -1,62 +1,9 @@
 const axios = require('axios');
 
-export function postRequirement(
-  token,
-  timeline,
-  numberOfPosition,
-  skillSet,
-  experience,
-  clientId,
-  additionalNote,
-) {
-  return axios
-    .post(
-      '/Requirement',
-      {
-        timeline,
-        number_of_position: numberOfPosition,
-        skill_set: skillSet,
-        experience,
-        client_id: clientId,
-        additional_note: additionalNote,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      },
-    )
-    .then((response) => response)
-    .catch((error) => error);
+export function postRequirement(requirementData) {
+  return axios.post('/Requirement', requirementData);
 }
 
-export function updateRequirement(
-  token,
-  requirementId,
-  timeline,
-  numberOfPosition,
-  skillSet,
-  experience,
-  clientId,
-  additionalNote,
-) {
-  return axios
-    .put(
-      `/Requirement/${requirementId}`,
-      {
-        timeline,
-        number_of_position: numberOfPosition,
-        skill_set: skillSet,
-        experience,
-        client_id: clientId,
-        additional_note: additionalNote,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      },
-    )
-    .then((response) => response)
-    .catch((error) => error);
+export function updateRequirement(requirementData) {
+  return axios.put(`/Requirement/${requirementId}`, requirementData);
 }
