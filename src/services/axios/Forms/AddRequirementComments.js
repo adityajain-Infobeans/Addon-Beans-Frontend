da7562e9b5e50349bb1d8398565b5deb4c50d8b1
Aditya Jain Infobeans <adityan.jain@infobeans.com>
2021-08-09 17:04:22 +0530

    new postComment,getRequirementIdData,updateStatus

--- src/services/axios/Forms/AddRequirementComments.js
+++ src/services/axios/Forms/AddRequirementComments.js
@@ -1,47 +1,13 @@
 const axios = require('axios');
 
-export function postComment(token, requirementId, comment) {
-  return axios
-    .post(
-      '/comment',
-      {
-        requirement_id: requirementId,
-        comment,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      },
-    )
-    .then((response) => response)
-    .catch((error) => error);
+export function postComment(ApiData) {
+  return axios.post('/comment', ApiData);
 }
 
-export function getRequirementIdData(token, requirementId) {
-  return axios
-    .get(`/Requirement/${requirementId}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-    .then((response) => response)
-    .catch((error) => error);
+export function getRequirementIdData(requirementId) {
+  return axios.get(`/Requirement/${requirementId}`);
 }
 
-export function updateStatus(token, requirementId, status) {
-  return axios
-    .put(
-      `/Requirement/${requirementId}`,
-      {
-        status,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      },
-    )
-    .then((response) => response)
-    .catch((error) => error);
+export function updateStatus(requirementId, status) {
+  return axios.put(`/Requirement/${requirementId}`, status);
 }
