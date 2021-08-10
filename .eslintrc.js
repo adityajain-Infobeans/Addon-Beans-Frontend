98be9b97b0f8127a7ac2c9bbc098b653cd680168
Aditya Jain Infobeans <adityan.jain@infobeans.com>
2021-08-10 17:50:53 +0530

    new linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,17 +15,17 @@ module.exports = {
     'linebreak-style': 0,
     'implicit-arrow-linebreak': 0,
     'operator-linebreak': 0,
-    'no-undef':0,
+    'no-undef': 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-infix-ops': 0,
+    camelcase: 0,
+    'import/prefer-default-export': 0,
   },
 
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true,
       },
