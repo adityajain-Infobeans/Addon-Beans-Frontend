8d0dee2377e4180eabef194532428796cf9e88c9
Aditya Jain Infobeans <adityan.jain@infobeans.com>
2021-08-11 18:05:37 +0530

    fixing api

--- src/services/axios/Forms/RequirementForm.js
+++ src/services/axios/Forms/RequirementForm.js
@@ -4,6 +4,6 @@ export function postRequirement(requirementData) {
   return axios.post('/Requirement', requirementData);
 }
 
-export function updateRequirement(requirementData) {
+export function updateRequirement(requirementId, requirementData) {
   return axios.put(`/Requirement/${requirementId}`, requirementData);
 }
